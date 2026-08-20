bda99c672b2635cb2294b4bf40886c5ff1d6feff
Koi-No-Yokan <joseph.jimenez108@pascualbravo.edu.co>
2026-08-19 22:19:55 -0500

    Update app_gradient.py

--- app_gradient.py
+++ app_gradient.py
@@ -140,10 +140,7 @@ def figura_convergencia(historial):
 # =========================================================
 with st.sidebar:
     st.header("⚙️ Parámetros")
-    st.caption(
-    f"Mínimo detectado automáticamente en ({MINIMO[0]:.2f}, {MINIMO[1]:.2f}) | "
-    "Gradiente aplicado: ∇f(x,y) = (2(x−3), 2(y+2))"
-)
+    st.caption(f"Mínimo detectado automáticamente en ({MINIMO[0]:.2f}, {MINIMO[1]:.2f})")
 
     eta = st.slider(
         "Tasa de aprendizaje (η)",
